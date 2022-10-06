a94d6e2460056c5e8500b0b120bd32dc311f9f11
jepthe <jepthe.hf@gmail.com>
2022-10-06 08:02:55 -0500

    delete Ejercicio5

--- ProfeNelly/Ciclos/Ejercicio5/src/App.java
+++
@@ -1,8 +0,0 @@
-/*5. Hacer un algoritmo que solicite N números y después de ellos se muestre cuál fue el mayor, cuál fue 
-el menor y el promedio de todos. Los ceros dados no se cuentan (se ignoran). N es dado por el usuario, 
-así como cada número. */
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
-}

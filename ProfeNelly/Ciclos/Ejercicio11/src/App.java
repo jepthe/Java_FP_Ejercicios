aa6ac6828928057819599652e355bf97d29c39e7
jepthe <jepthe.hf@gmail.com>
2022-10-05 23:57:57 -0500

    Ejercicio11 (fibonacci)

--- ProfeNelly/Ciclos/Ejercicio11/src/App.java
+++ ProfeNelly/Ciclos/Ejercicio11/src/App.java
@@ -1,8 +1,26 @@
 /*11.  Hacer un algoritmo que imprima la siguiente sucesión de números:
 0,1,1,2,3,5,8,13,21,34,55,89,144,233,377...  
- Observe  que  la  sucesión  comienza con los números 0 y 1, y a partir de éstos, los siguientes números se forman con la suma de los dos anteriores. El usuario debe ingresar la cantidad de valores que quiere ver, por ejemplo si el usuario indica que quiere ver 5 números, solo se imprimiría la sucesión 0,1,1,2,3*/
+ Observe  que  la  sucesión  comienza con los números 0 y 1, y a partir de éstos, 
+ los siguientes números se forman con la suma de los dos anteriores. 
+ El usuario debe ingresar la cantidad de valores que quiere ver, por ejemplo 
+ si el usuario indica que quiere ver 5 números, solo se imprimiría la 
+ sucesión 0,1,1,2,3*/
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner leer=new Scanner(System.in);
+        int a=0, b=1, c=0;
+        System.out.print("Indicar cantidad de numeros visibles: ");
+        int n=leer.nextInt();//numero de iteraciones
+
+        for (int i = 0; i < n-1; i++) {
+            if (a==0 && b==1 && c==0) {//condiciona primera iteracion para que sea 0
+                System.out.print("0");
+            }
+            a=b;
+            b=c;
+            c=a+b;
+            System.out.print(", "+c);         
+        }
     }
 }

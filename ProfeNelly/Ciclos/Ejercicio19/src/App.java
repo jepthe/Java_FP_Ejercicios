eda412600e797d08f69bbcdaaab8559661f798d7
jepthe <jepthe.hf@gmail.com>
2022-10-06 13:11:33 -0500

    Ejercicio19 creado

---
+++ ProfeNelly/Ciclos/Ejercicio19/src/App.java
@@ -0,0 +1,44 @@
+/*19. (Hasta ver tema de cadenas y caracteres en java). Hacer un programa con lo siguiente: un menú con el nombre 
+de figuras de donde se pueda elegir la que se desea ver en ese momento. El usuario indicará en un inicio el 
+caracter con el que desea ver la figura. Dentro del menú debe haber una opción para cambiar el caracter
+cuando se deseé. Después de imprimir una figura, volver al menú. El programa continua hasta que se elija la 
+opción terminar del menú. Utiliza ‘\n’ para imprimir el caracter en renglones sucesivos.
+1) * 2) *
+* * * * * *
+* * * * * * * * * *
+* * * * * * * * * * * * * *
+* * * * * * * * * * * * * * * * * *
+* * * * * * *
+* * * * * 3)
+* * * * * * * * * * * *
+* * * * * * * *
+* * * * *
+* * *
+4) * 5) * *
+* * * * 6)
+* * * * * * * * * * * * *
+* * * * * * * * * * * * *
+* * * * * * * * * * * * *
+* * * * * * * * *
+* * * * * * * * *
+* * * * * * * * *
+* * * * * * * * *
+7)
+* * * * * * * *
+* * * * * * * *
+* * * * * * * *
+* * * * * * * *
+* * * * * * * *
+* * * * * * * *
+* * * * * * * *
+* * * * * * * *
+* * * * * * * *
+* * * * * * * *
+* * * * * * * *
+
+*/
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
